0ef18878c8c767adfbeb08beae2e92d9c12590da
Daryl Antony <daryl@commoncode.com.au>
2013-12-09 17:16:57 +1100

    add staging role

--- app/roles.py
+++ app/roles.py
@@ -1,5 +1,9 @@
 vhosts = {
     # Staging
+    'daryl.commoncode.com.au': {
+        'hosts': ['aurelius.commoncode.com.au'],
+    },
+    # Production
     'daryl.io': {
         'hosts': ['aurelius.commoncode.com.au'],
     }
